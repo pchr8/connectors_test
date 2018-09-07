bc1d37b9dd1785144b5eb3292b7fdc2d3a9310d6
Pchr8 <pchr8@anche.no>
2018-09-07 15:18:07 +0300

    added age check, changed wording

--- 1.py
+++ 1.py
@@ -56,23 +56,37 @@ def get_db():
 def workon(request):
     """ Get user input, save it, and send to thank you page """
     error=None;
+
     name = request.form.get('myname')
-    age = request.form.get('age')
+    fage = request.form.get('age')
     gender = request.form.get('gender')
-    noshare = request.form.get('noshare')
+    fnoshare = request.form.get('noshare')
     
     if not name:
         error='Username is required'; 
     if not gender:
+        # TODO: Sanitize it based on expected values
         gender='na'
-    if not age:
-        age=None;
-    if not noshare:
-        ns=False;
-    elif (noshare=="noshare"):
-        ns=True;
+    if not fage:
+        age="N/A";
     else:
-        ns=False;
+        try:
+            # TODO: something smarter
+            # Do I have to be so aggressive?
+            t=int(fage)
+            if (t<120):
+                age=str(t)
+            else:
+                age="N/A"
+        except ValueError:
+            age="N/A"
+
+    if not fnoshare:
+        noshare=False;
+    elif (fnoshare=="noshare"):
+        noshare=True;
+    else:
+        noshare=False;
 
     l1 = request.form.getlist("nlist1[]");
     l1len=len(l1);
@@ -87,7 +101,7 @@ def workon(request):
     # Save the results
     db = get_db()
     db.execute('insert into entries (name, age, gender, nlist1, n1, nlist2, n2, ip, date, noshare) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-               [name, age, gender, str(l1), l1len, str(l2), l2len,  "127.9.9.91", datetime.now(), ns])
+               [name, age, gender, str(l1), l1len, str(l2), l2len,  "127.9.9.91", datetime.now(), noshare])
     db.commit()
     if error is None:
         return render_template('res.html', name=name, nn1=l1len, nn2=l2len);
